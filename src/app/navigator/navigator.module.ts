2e4b2ddf368c4a5359ae1807977881ed10cae6f0
qaman88 <qaman88@hotmail.com>
2021-04-18 19:51:51 +0200

    Social links
    Features:
    * Social links
    
    Fixes:
    * Bug fixing and code optimazation
    * Code comment
    * Code brush up
    * Code clean up
    
    Drops:
    *

--- src/app/navigator/navigator.module.ts
+++ src/app/navigator/navigator.module.ts
@@ -2,9 +2,13 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {NavigatorComponent} from "./navigator.component";
 import {BrowserModule} from "@angular/platform-browser";
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { FootbarComponent } from './footbar/footbar.component';
+import { SociallinksComponent } from './sociallinks/sociallinks.component';
 
 @NgModule({
-	declarations: [NavigatorComponent],
+	declarations: [NavigatorComponent, ToolbarComponent, ContentComponent, FootbarComponent, SociallinksComponent],
 	imports: [CommonModule, BrowserModule],
 	bootstrap: [NavigatorComponent],
 })
